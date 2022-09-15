ce97accaeb461874dfdca77f14f1cf79dfbf90a4
Altaf Husain <=>
2022-09-15 14:58:33 +0530

    migrations

--- django_pages/django_pages/settings.py
+++ django_pages/django_pages/settings.py
@@ -76,8 +76,17 @@ WSGI_APPLICATION = "django_pages.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'django_pages',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Study@2021',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
